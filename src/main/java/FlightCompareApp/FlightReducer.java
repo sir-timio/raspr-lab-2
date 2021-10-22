639806cc8aeac0eae75464bd040ced5a7de0247f
sir-timio <t-ionov@list.ru>
2021-10-22 16:54:37 +0300

    1634910877

--- src/main/java/FlightCompareApp/FlightReducer.java
+++ src/main/java/FlightCompareApp/FlightReducer.java
@@ -3,5 +3,12 @@ package FlightCompareApp;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class FlightReducer extends Reducer<FlightWritableComparable, Text, Text, Text>{
+
+    @Override
+    protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+    }
 }
