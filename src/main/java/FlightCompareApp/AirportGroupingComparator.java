3bb0b0fa75f7f41ca948a10049045778ee0c2c32
sir-timio <t-ionov@list.ru>
2021-10-22 16:45:02 +0300

    1634910302

--- src/main/java/FlightCompareApp/AirportGroupingComparator.java
+++ src/main/java/FlightCompareApp/AirportGroupingComparator.java
@@ -1,4 +1,18 @@
 package FlightCompareApp;
 
-public class AirportGroupingComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+
+public class AirportGroupingComparator extends WritableComparator{
+
+    public AirportGroupingComparator () {
+        super(FlightWritableComparable.class, true);
+    }
+
+    @Override
+    protected int compare(WritableComparable a1, WritableComparable a2) {
+        FlightWritableComparable airport1 = (FlightWritableComparable) a1;
+        FlightWritableComparable
+    }
 }
