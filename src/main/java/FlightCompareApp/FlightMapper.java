cb09f8df8d56087fead232fbd0cb5863434080ea
sir-timio <t-ionov@list.ru>
2021-10-22 22:05:39 +0300

    1634929539

--- src/main/java/FlightCompareApp/FlightMapper.java
+++ src/main/java/FlightCompareApp/FlightMapper.java
@@ -18,7 +18,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableCompa
             String[] row = value.toString().split(DELIMITER);
             String rawDelay = row[ARR_DELAY_COLUMN];
 
-            if (!rawDelay.isEmpty()){
+            if (!rawDelay.isEmpty() && Float.parseFloat(rawDelay) != 0){
                 int destAirportID = Integer.parseInt(row[DEST_AIRPORT_ID_COLUMN]);
 
                 context.write(new FlightWritableComparable(destAirportID, DATA_TYPE),
