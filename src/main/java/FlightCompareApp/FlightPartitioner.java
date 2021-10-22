c14c54f5dbf415243a3109c2d5be19da27369b16
sir-timio <t-ionov@list.ru>
2021-10-22 13:01:10 +0300

    1634896870

--- src/main/java/FlightCompareApp/FlightPartitioner.java
+++ src/main/java/FlightCompareApp/FlightPartitioner.java
@@ -1,2 +1,9 @@
-package FlightCompareApp;public class FlightPartitioner {
+package FlightCompareApp;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class FlightPartitioner extends Partitioner{
+    @Override
+    public int getPartition(Object o, Object o2, int i) {
+        return 0;
+    }
 }
