f38f447c3a65d8d006e2ea975445252ee8b793a7
sir-timio <t-ionov@list.ru>
2021-10-22 12:44:33 +0300

    1634895873

--- src/main/java/FlightCompareApp/FlightMapper.java
+++ src/main/java/FlightCompareApp/FlightMapper.java
@@ -6,6 +6,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightMapper extends Mapper<IntWritable, Text, FlightWritableComparable, Text>{
     private static String DELIMITER = ",";
-    private static int AIRPORT_ID_COLUMN =  14;
+    private static int DEST_AIRPORT_ID =  14;
     private static int CANCELED_COLUMN = 
 }
