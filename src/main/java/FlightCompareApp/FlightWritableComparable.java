b3215909e615ebced95aa7218d02c95466851208
sir-timio <t-ionov@list.ru>
2021-10-22 12:14:30 +0300

    1634894070

--- src/main/java/FlightCompareApp/FlightWritableComparable.java
+++ src/main/java/FlightCompareApp/FlightWritableComparable.java
@@ -1,2 +1,19 @@
-package FlightCompareApp;public class FlightWritableComparable {
+package FlightCompareApp;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlightWritableComparable implements WritableComparable{
+    
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
