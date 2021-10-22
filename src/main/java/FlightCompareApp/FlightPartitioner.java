d33c18e9ad0f4ba0ae12d9c82314ebaf87ed078f
sir-timio <t-ionov@list.ru>
2021-10-22 16:03:42 +0300

    1634907822

--- src/main/java/FlightCompareApp/FlightPartitioner.java
+++ src/main/java/FlightCompareApp/FlightPartitioner.java
@@ -2,10 +2,10 @@ package FlightCompareApp;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.Text;
 
-public class FlightPartitioner extends Partitioner<AirportWritableComparable, Text>{
+public class FlightPartitioner extends Partitioner<FlightWritableComparable, Text>{
 
     @Override
-    public int getPartition(AirportWritableComparable flightWritableComparable, Text text, int numPartitions) {
+    public int getPartition(FlightWritableComparable flightWritableComparable, Text text, int numPartitions) {
        return flightWritableComparable.getAirportID() % numPartitions;
     }
 }
