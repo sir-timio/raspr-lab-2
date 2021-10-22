6130188f73eb086a2a65109cb4202936ce22328e
sir-timio <t-ionov@list.ru>
2021-10-22 12:09:42 +0300

    1634893782

--- src/main/java/FlightCompareApp/AirportWritable.java
+++ src/main/java/FlightCompareApp/AirportWritable.java
@@ -6,13 +6,18 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportWritable implements Writable{
+    private int code;
+    private String description;
+
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-        
+        dataOutput.writeInt(code);
+        dataOutput.writeBytes(description);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-
+        this.code = dataInput.readInt();
+        this.description = dataInput.readLine();
     }
 }
