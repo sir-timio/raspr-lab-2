2b315cb291eaa0f3a3e29c286591413e661656c0
sir-timio <t-ionov@list.ru>
2021-10-22 17:31:47 +0300

    1634913107

--- src/main/java/FlightCompareApp/AirportStatsApp.java
+++ src/main/java/FlightCompareApp/AirportStatsApp.java
@@ -7,9 +7,11 @@ import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
+import java.io.IOException;
+
 public class AirportStatsApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 3) {
             System.err.println("flights airports output");
             System.exit(-1);
