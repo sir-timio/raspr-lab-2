ef7208226de699a687d1ee993a54ed90f7c82462
sir-timio <t-ionov@list.ru>
2021-10-22 13:30:08 +0300

    1634898608

--- src/main/java/FlightCompareApp/AirportMapper.java
+++ src/main/java/FlightCompareApp/AirportMapper.java
@@ -1,2 +1,8 @@
-package FlightCompareApp;public class AirportMapper {
+package FlightCompareApp;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class AirportMapper extends Mapper<LongWritable, Text, >{
 }
