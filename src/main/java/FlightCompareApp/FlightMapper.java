e15d626fef5e0e02b17178206bccb96ba25de39d
sir-timio <t-ionov@list.ru>
2021-10-22 12:45:33 +0300

    1634895933

--- src/main/java/FlightCompareApp/FlightMapper.java
+++ src/main/java/FlightCompareApp/FlightMapper.java
@@ -8,5 +8,7 @@ public class FlightMapper extends Mapper<IntWritable, Text, FlightWritableCompar
     private static String DELIMITER = ",";
     private static int DEST_AIRPORT_ID_COLUMN =  14;
     private static int CANCELED_COLUMN = 19;
-    private static int ARR_DELAY_COLUMN = 
+    private static int ARR_DELAY_COLUMN = 17;
+    private static int ARR_DELAY_NEW_COLUMN = 18;
+    
 }
