e413cbd737950cbdc92415b6d47fdb647115ebcd
sir-timio <t-ionov@list.ru>
2021-10-22 21:40:52 +0300

    1634928052

--- src/main/java/FlightCompareApp/AirportStatsApp.java
+++ src/main/java/FlightCompareApp/AirportStatsApp.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 public class AirportStatsApp {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
         if (args.length != 3) {
             System.err.println("flights airports output");
             System.exit(-1);
