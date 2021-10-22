428958ba5c2b6e5898596bf28ccd8dc8977458b8
sir-timio <t-ionov@list.ru>
2021-10-22 16:22:32 +0300

    1634908952

--- src/main/java/FlightCompareApp/AirportMapper.java
+++ src/main/java/FlightCompareApp/AirportMapper.java
@@ -5,4 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class AirportMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text>{
+
+    private static String DELIMETER = ",";
+    private static int AIRPORT
 }
