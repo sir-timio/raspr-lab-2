4401f0062d46c61fc8fac6959015eac707875b96
sir-timio <t-ionov@list.ru>
2021-10-22 16:24:21 +0300

    1634909061

--- src/main/java/FlightCompareApp/AirportMapper.java
+++ src/main/java/FlightCompareApp/AirportMapper.java
@@ -4,9 +4,19 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text>{
 
     private static String DELIMETER = ",";
     private static int AIRPORT_CODE_COLUMN = 0;
-    private static int 
+    private static int NAME_COLUMN = 1;
+    private static int DATA_TYPE = 0;
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() != 0) {
+            
+        }
+    }
 }
