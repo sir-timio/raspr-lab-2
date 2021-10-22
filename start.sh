42ab68973a6aab521f28b419744de35dfb88b5df
sir-timio <t-ionov@list.ru>
2021-10-22 17:43:17 +0300

    1634913797

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ mvn package
 hadoop fs -copyFromLocal data/Flights.csv
 hadoop fs -copyFromLocal data/Airports.csv
 export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar
-hadoop 
+hadoop FlightCompareApp data/Flights.csv data/Airports.csv output
+hadoop fs -copyToLocal output
