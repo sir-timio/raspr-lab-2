49a356a91a9bdf5b0367aaf23c06099ab84a2e2e
sir-timio <t-ionov@list.ru>
2021-10-22 17:35:45 +0300

    1634913345

--- src/main/java/FlightCompareApp/AirportStatsApp.java
+++ src/main/java/FlightCompareApp/AirportStatsApp.java
@@ -6,12 +6,13 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 
 public class AirportStatsApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
         if (args.length != 3) {
             System.err.println("flights airports output");
             System.exit(-1);
@@ -23,6 +24,17 @@ public class AirportStatsApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightMapper.class);
 
+        job.setMapOutputKeyClass(FlightWritableComparable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setPartitionerClass(FlightPartitioner.class);
+        job.setGroupingComparatorClass(AirportGroupingComparator.class);
+        job.setReducerClass(FlightReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputKeyClass(Text.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0:1);
 
     }
 }
