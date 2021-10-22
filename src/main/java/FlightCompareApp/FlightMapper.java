35f7470a0cd705445ed44b6a35a3c9b1830e0ddf
sir-timio <t-ionov@list.ru>
2021-10-22 12:47:07 +0300

    1634896027

--- src/main/java/FlightCompareApp/FlightMapper.java
+++ src/main/java/FlightCompareApp/FlightMapper.java
@@ -1,14 +1,17 @@
 package FlightCompareApp;
 
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class FlightMapper extends Mapper<IntWritable, Text, FlightWritableComparable, Text>{
+public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text>{
     private static String DELIMITER = ",";
     private static int DEST_AIRPORT_ID_COLUMN =  14;
     private static int CANCELED_COLUMN = 19;
     private static int ARR_DELAY_COLUMN = 17;
     private static int ARR_DELAY_NEW_COLUMN = 18;
 
+    @Override
+    protected void map(IntWritable key, )
 }
