b960cf19e13cf2093c01dd7bb376300eaa5ab786
sir-timio <t-ionov@list.ru>
2021-10-22 16:52:47 +0300

    1634910767

--- src/main/java/FlightCompareApp/FlightReducer.java
+++ src/main/java/FlightCompareApp/FlightReducer.java
@@ -3,5 +3,5 @@ package FlightCompareApp;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class FlightReducer {
+public class FlightReducer extends Reducer<FlightWritableComparable, Text, Text, Text>{
 }
