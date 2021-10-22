0faa69e0337a8759f6bbb19b1c08e8f6f6dd6347
sir-timio <t-ionov@list.ru>
2021-10-22 17:25:13 +0300

    1634912713

--- src/main/java/FlightCompareApp/AirportStatsApp.java
+++ src/main/java/FlightCompareApp/AirportStatsApp.java
@@ -2,4 +2,12 @@ package FlightCompareApp;
 
 public class AirportStatsApp {
 
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("flights airports output");
+            System.exit(-1);
+        }
+
+        Job 
+    }
 }
