785e40fced6cdb742072d3e661cbb01a6e8bfc1c
sir-timio <t-ionov@list.ru>
2021-10-22 17:00:36 +0300

    1634911236

--- src/main/java/FlightCompareApp/FlightReducer.java
+++ src/main/java/FlightCompareApp/FlightReducer.java
@@ -4,12 +4,15 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class FlightReducer extends Reducer<FlightWritableComparable, Text, Text, Text>{
 
     @Override
     protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        Text airportName = new Text(key.toString());
+        Iterator<Text> iterator = values.iterator();
+        Text airportName = new Text(iterator.next());
+
         
     }
 }
