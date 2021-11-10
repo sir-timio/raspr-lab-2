ed24f7d4b7efa2452c186747f3d5549b65a6c75a
sir-timio <t-ionov@list.ru>
2021-11-10 13:58:37 +0300

    Scripted auto-commit on change (2021-11-10 13:58:37) by gitwatch.sh

---
+++ lab2/start.sh
@@ -0,0 +1,31 @@
+mvn package
+
+stop-yarn.sh
+stop-dfs.sh
+
+rm -rf ~/hdata/namenode
+rm -rf ~/hdata/datanode
+
+mkdir ~/hdata/namenode
+mkdir ~/hdata/datanode
+
+hdfs namenode -format
+
+start-dfs.sh
+start-yarn.sh
+
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/timur
+
+hadoop fs -rm -r -f  hdfs://localhost:9000/user/timur/output
+
+hadoop fs -copyFromLocal data/flights.csv
+hadoop fs -copyFromLocal data/airports.csv
+
+export HADOOP_CLASSPATH=target/FlightCompareApp-1.0.jar
+
+hadoop FlightCompareApp.AirportStatsApp flights.csv airports.csv output
+
+rm -rf output
+mkdir output
+hadoop fs -copyToLocal output
