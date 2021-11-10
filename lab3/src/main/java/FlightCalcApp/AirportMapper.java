84df01271ee6020013761a44d7fb6be6209054f6
sir-timio <t-ionov@list.ru>
2021-11-10 18:47:37 +0300

    Scripted auto-commit on change (2021-11-10 18:47:37) by gitwatch.sh

--- lab3/src/main/java/FlightCalcApp/AirportMapper.java
+++ lab3/src/main/java/FlightCalcApp/AirportMapper.java
@@ -4,4 +4,15 @@ import java.io.Serializable;
 
 public class AirporMapper implements Serializable {
 
+    private static final String DELIMETER = ",";
+    private static final int AIRPORT_CODE_COLUMN = 0;
+    private static final int NAME_COLUMN = 1;
+    private static final int DATA_TYPE = 0;
+    private static final String REDUNTANT_QUOT = "\"";
+
+    protected String removeQuots(String s){
+        return s.replace(REDUNTANT_QUOT, "");
+    }
+    
+
 }
