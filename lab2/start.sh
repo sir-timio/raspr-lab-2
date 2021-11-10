fcc70dd400bbf05d179b32887d6ce8c743b8b6af
sir-timio <t-ionov@list.ru>
2021-11-10 14:05:47 +0300

    Scripted auto-commit on change (2021-11-10 14:05:47) by gitwatch.sh

--- lab2/start.sh
+++ lab2/start.sh
@@ -17,7 +17,7 @@ start-yarn.sh
 hdfs dfs -mkdir /user
 hdfs dfs -mkdir /user/timur
 
-hadoop fs -rm -r -f  hdfs://localhost:9000/user/timur/output
+hadoop fs -rm -r -f  hdfs://localhost:9000/user/parallels/output
 
 hadoop fs -copyFromLocal data/flights.csv
 hadoop fs -copyFromLocal data/airports.csv
