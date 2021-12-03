80c17e322f15a9ebfacf03dbd46187712c0a9dcd
sir-timio <t-ionov@list.ru>
2021-12-03 21:02:08 +0300

    Scripted auto-commit on change (2021-12-03 21:02:08) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightApp.java
+++ lab3/src/main/java/FlightApp/FlightApp.java
@@ -2,5 +2,5 @@ package FlightApp;
 import org.apache.spark.SparkConf;
 
 public class FlightApp {
-    SparkConf conf = new SparkConf().setAppName("")
+    SparkConf conf = new SparkConf().setAppName("");
 }
