5d8d94f1f50e388dde261a7dc3cc7f9f50247a06
sir-timio <t-ionov@list.ru>
2021-12-03 22:59:11 +0300

    Scripted auto-commit on change (2021-12-03 22:59:11) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightApp.java
+++ lab3/src/main/java/FlightApp/FlightApp.java
@@ -1,7 +1,9 @@
 package FlightApp;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class FlightApp {
     private static final String PATH_TO_FLIGHTS = "flights.csv";
@@ -9,6 +11,7 @@ public class FlightApp {
     SparkConf conf = new SparkConf().setAppName("FlightApp");
     JavaSparkContext sc = new JavaSparkContext(conf);
 
-    JavaRDD<String> flightFile = sc.textFile("flights.csv");
+    JavaRDD<String> flightFile = sc.textFile(PATH_TO_FLIGHTS);
+    JavaPairRDD<Tuple2<Integer, Integer>, FlightSerializable>
 
 }
