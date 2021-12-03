14ce10581821ee1b26a1c03d1dd6b4ec71782538
sir-timio <t-ionov@list.ru>
2021-12-03 22:46:56 +0300

    Scripted auto-commit on change (2021-12-03 22:46:56) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightSerializable.java
+++ lab3/src/main/java/FlightApp/FlightSerializable.java
@@ -3,6 +3,7 @@ package FlightApp;
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
-    private float delay;
-    private boolean isCancelled;
+    private final float delay;
+    private final boolean isCancelled;
+
 }
