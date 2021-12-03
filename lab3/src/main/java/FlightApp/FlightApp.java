a2dc2d9c0cefa02b2cc8a59f5fd26086a158771f
sir-timio <t-ionov@list.ru>
2021-12-03 22:29:11 +0300

    Scripted auto-commit on change (2021-12-03 22:29:11) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightApp.java
+++ lab3/src/main/java/FlightApp/FlightApp.java
@@ -1,6 +1,12 @@
 package FlightApp;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightApp {
     SparkConf conf = new SparkConf().setAppName("FlightApp");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+
+    JavaRDD<String> flightFile = sc.textFile("flights.csv");
+    
 }
