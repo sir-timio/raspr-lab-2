721b5e8f6071ba8284346854f893e47f6f99f2d6
sir-timio <t-ionov@list.ru>
2021-12-04 02:39:06 +0300

    Scripted auto-commit on change (2021-12-04 02:39:06) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightMapper.java
+++ lab3/src/main/java/FlightApp/FlightMapper.java
@@ -26,6 +26,7 @@ public class FlightMapper {
         int originAirportId = Integer.parseInt(row[ORIGIN_AIRPORT_ID_COLUMN]);
         int destAirportId = Integer.parseInt(row[DEST_AIRPORT_ID_COLUMN]);
 
-        return new Tuple2<Tuple2<Integer, Integer>, FlightApp.FlightSerializable>(delay, isCancelled)
+        return new Tuple2<Tuple2<Integer, Integer>,
+                FlightApp.FlightSerializable>(delay, isCancelled)
     }
 }
