df83543612b75f19f557e03d6d21fba2436b979f
sir-timio <t-ionov@list.ru>
2021-12-03 23:04:17 +0300

    Scripted auto-commit on change (2021-12-03 23:04:17) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightApp.java
+++ lab3/src/main/java/FlightApp/FlightApp.java
@@ -16,6 +16,6 @@ public class FlightApp {
 
     JavaRDD<String> airportsFIle = sc.textFile(PATH_TO_AIRPORTS);
     JavaPairRDD<Integer, String> airports = airportsFIle
-            .mapToPair(AirportMapper::processRow);
+                                            .mapToPair(AirportMapper::processRow);
 
 }
