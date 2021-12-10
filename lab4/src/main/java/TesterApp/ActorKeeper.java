5c263988612a6885be9c6e9ad9013620dfa70631
sir-timio <t-ionov@list.ru>
2021-12-10 22:14:31 +0300

    Scripted auto-commit on change (2021-12-10 22:14:31) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorKeeper.java
+++ lab4/src/main/java/TesterApp/ActorKeeper.java
@@ -5,6 +5,7 @@ import akka.actor.AbstractActor;
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ActorKeeper extends AbstractActor {
     private Map<String, ArrayList<TestResult>> results = new HashMap<>();
