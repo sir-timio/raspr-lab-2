d6618631f91b9b511f00f129fe52f549fdaf0e93
sir-timio <t-ionov@list.ru>
2021-12-10 22:22:00 +0300

    Scripted auto-commit on change (2021-12-10 22:22:00) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorTester.java
+++ lab4/src/main/java/TesterApp/ActorTester.java
@@ -2,7 +2,9 @@ package TesterApp;
 
 import akka.actor.AbstractActor;
 
+import javax.script.ScriptException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ActorTester extends AbstractActor {
     private static final String EVAL_ENGINE = "nashorn";
@@ -11,6 +13,8 @@ public class ActorTester extends AbstractActor {
     private static final String ERROR_STATUS = "ERROR";
 
 
-    private String evalJS(String jscript, String functionName, ArrayList<Object> params) throws 
+    private String evalJS(String jscript, String functionName, ArrayList<Object> params) throws ScriptException, NoSuchMethodException {
+        
+    }
 
 }
