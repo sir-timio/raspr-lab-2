bd49250ac5c73344956c1e797d1fa3fba3840381
sir-timio <t-ionov@list.ru>
2021-12-10 22:02:28 +0300

    Scripted auto-commit on change (2021-12-10 22:02:28) by gitwatch.sh

--- lab4/src/main/java/TesterApp/Test.java
+++ lab4/src/main/java/TesterApp/Test.java
@@ -5,7 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Test {
     @JsonProperty
     private final String testName;
+
+    @JsonProperty
     private final Object[] params;
+
+    @JsonProperty
     private final String expectedResult;
 
     public Test() {
