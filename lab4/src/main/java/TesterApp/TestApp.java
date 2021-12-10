39e9503c9097ef520cf010fadb0f59071efc75ad
sir-timio <t-ionov@list.ru>
2021-12-10 21:40:27 +0300

    Scripted auto-commit on change (2021-12-10 21:40:27) by gitwatch.sh

--- lab4/src/main/java/TesterApp/TestApp.java
+++ lab4/src/main/java/TesterApp/TestApp.java
@@ -1,12 +1,17 @@
 package TesterApp;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.server.AllDirectives;
+import akka.stream.ActorMaterializer;
 
 public class TestApp extends AllDirectives {
     private static final String ACTOR_SYSTEM_NAME = "JS_TESTER";
 
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
+
+        Http http = Http.get(actorSystem);
+        ActorMaterializer actorMaterializer = ActorMaterializer.create(ActorSystem);
         
     }
 }
