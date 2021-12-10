8ad42274e9e8a3bcf6860d782c98a6ed8c700b27
sir-timio <t-ionov@list.ru>
2021-12-11 00:03:29 +0300

    Scripted auto-commit on change (2021-12-11 00:03:29) by gitwatch.sh

--- lab4/src/main/java/TesterApp/TestResultStore.java
+++ lab4/src/main/java/TesterApp/TestResultStore.java
@@ -3,4 +3,7 @@ package TesterApp;
 public class TestResultStore {
     private final String packageId;
     private final TestResult result;
+
+    public TestResultStore(String packageId, String status, String testName,
+                           String expectedResult, String receivedResult)
 }
