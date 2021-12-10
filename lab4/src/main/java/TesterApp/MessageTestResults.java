1bc413035acec1dbb1c8f7872f84af1a63f8f176
sir-timio <t-ionov@list.ru>
2021-12-11 00:30:33 +0300

    Scripted auto-commit on change (2021-12-11 00:30:33) by gitwatch.sh

--- lab4/src/main/java/TesterApp/MessageTestResults.java
+++ lab4/src/main/java/TesterApp/MessageTestResults.java
@@ -1,10 +1,19 @@
 package TesterApp;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 
 public class MessageTestResults {
     private final String packageId;
     public final ArrayList<TestResult> results;
 
-    @
+    @JsonCreator
+    public MessageReturnResults(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("results") ArrayList<TestResult> results) {
+        this.packageId = packageId;
+        this.results = results;
+    }
 }
