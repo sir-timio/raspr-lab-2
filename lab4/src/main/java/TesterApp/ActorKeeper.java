27ea4a570fb3c4603a88d7b2d720f5934232c39b
sir-timio <t-ionov@list.ru>
2021-12-10 21:44:04 +0300

    Scripted auto-commit on change (2021-12-10 21:44:04) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorKeeper.java
+++ lab4/src/main/java/TesterApp/ActorKeeper.java
@@ -1,4 +1,15 @@
 package TesterApp;
 
-public class ActorKeeper {
+import akka.actor.AbstractActor;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class ActorKeeper extends AbstractActor {
+    private Map<String, List<TestResult>> results = new HashMap<>();
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match()
+    }
 }
