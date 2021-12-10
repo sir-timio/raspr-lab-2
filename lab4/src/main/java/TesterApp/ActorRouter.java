11d8fae52fd3750464a9bbae76fe16d59d886ed9
sir-timio <t-ionov@list.ru>
2021-12-10 22:32:34 +0300

    Scripted auto-commit on change (2021-12-10 22:32:34) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorRouter.java
+++ lab4/src/main/java/TesterApp/ActorRouter.java
@@ -1,8 +1,16 @@
 package TesterApp;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.routing.Router;
 
 public class ActorRouter extends AbstractActor {
     private static final int NUM_WORKERS = 3;
-    
+
+    private final ActorRef keeper;
+    private final Router router;
+
+    {
+        keeper = getContext().actorOf()
+    }
 }
