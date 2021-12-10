517424fd5a872697a0317bb8865992cc0e17a598
sir-timio <t-ionov@list.ru>
2021-12-11 00:11:22 +0300

    Scripted auto-commit on change (2021-12-11 00:11:22) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorTester.java
+++ lab4/src/main/java/TesterApp/ActorTester.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class ActorTester extends AbstractActor {
     private static final String EVAL_ENGINE = "nashorn";
     private static final String PASSED_STATUS = "OK";
-    private static final String FAILING_STATUS = "FAILED";
+    private static final String FAILED_STATUS = "FAILED";
     private static final String ERROR_STATUS = "ERROR";
 
     @Override
@@ -39,7 +39,7 @@ public class ActorTester extends AbstractActor {
                     messageTest.getFunctionName(),
                     messageTest.getTest().getParams()
             );
-            status = expected.equals(evaluated) ? PASSED_STATUS : FAILING_STATUS
+            status = expected.equals(evaluated) ? PASSED_STATUS : FAILED_STATUS
         } catch (ScriptException | NoSuchMethodException e) {
             e.printStackTrace();
         }
