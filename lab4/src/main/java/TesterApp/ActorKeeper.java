175e6aed55f47975861032283168151bbd47bd0d
sir-timio <t-ionov@list.ru>
2021-12-10 22:14:22 +0300

    Scripted auto-commit on change (2021-12-10 22:14:22) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorKeeper.java
+++ lab4/src/main/java/TesterApp/ActorKeeper.java
@@ -3,10 +3,11 @@ package TesterApp;
 import akka.actor.AbstractActor;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ActorKeeper extends AbstractActor {
-    private Map<String, List<TestResult>> results = new HashMap<>();
+    private Map<String, ArrayList<TestResult>> results = new HashMap<>();
 
     @Override
     public Receive createReceive() {
