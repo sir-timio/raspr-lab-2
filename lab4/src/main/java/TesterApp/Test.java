4773acaa572b4a6019fe16534e1e5aeaa5f7de60
sir-timio <t-ionov@list.ru>
2021-12-10 22:02:20 +0300

    Scripted auto-commit on change (2021-12-10 22:02:20) by gitwatch.sh

--- lab4/src/main/java/TesterApp/Test.java
+++ lab4/src/main/java/TesterApp/Test.java
@@ -1,11 +1,14 @@
 package TesterApp;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test {
+    @JsonProperty
     private final String testName;
     private final Object[] params;
     private final String expectedResult;
 
     public Test() {
-        
+
     }
 }
