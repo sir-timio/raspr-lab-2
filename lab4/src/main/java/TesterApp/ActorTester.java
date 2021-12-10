f44cf381d41a6f0150d105d6e3d87a2050ceb6d3
sir-timio <t-ionov@list.ru>
2021-12-10 22:19:11 +0300

    Scripted auto-commit on change (2021-12-10 22:19:11) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorTester.java
+++ lab4/src/main/java/TesterApp/ActorTester.java
@@ -6,6 +6,8 @@ public class ActorTester extends AbstractActor {
     private static final String EVAL_ENGINE = "nashorn";
     private static final String PASSED_STATUS = "OK";
     private static final String FAILING_STATUS = "FAILED";
-    private static final String ERROR_STATUS = "ERROR"
+    private static final String ERROR_STATUS = "ERROR";
+
+    
 
 }
