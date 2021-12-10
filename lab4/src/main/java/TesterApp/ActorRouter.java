8dd09f930c45de335dc4236c6d0e23fdad2d0f87
sir-timio <t-ionov@list.ru>
2021-12-10 22:35:32 +0300

    Scripted auto-commit on change (2021-12-10 22:35:32) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorRouter.java
+++ lab4/src/main/java/TesterApp/ActorRouter.java
@@ -3,7 +3,11 @@ package TesterApp;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.routing.Routee;
 import akka.routing.Router;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActorRouter extends AbstractActor {
     private static final int NUM_WORKERS = 3;
@@ -15,6 +19,11 @@ public class ActorRouter extends AbstractActor {
         keeper = getContext().actorOf(Props.create(ActorKeeper.class));
         getContext().watch(keeper);
 
-        
+        List<Routee> routees = new ArrayList<>();
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            ActorRef 
+        }
+
+
     }
 }
