03c5c4de55d9724466673b5a2814f6b36731958a
sir-timio <t-ionov@list.ru>
2021-12-10 16:31:08 +0300

    Scripted auto-commit on change (2021-12-10 16:31:08) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightApp.java
+++ lab3/src/main/java/FlightApp/FlightApp.java
@@ -40,7 +40,10 @@ public class FlightApp {
         JavaRDD<String> statRDD = flightsStat.map(
                 flightsStat -> {
                     Map<Integer, String> airportsBroadcastedMap = airportsBroadcasted.value();
-
+                    String origAirportName = airportsBroadcastedMap.get(flightsStat._1._1);
+                    String destAirportName = airportsBroadcastedMap.get(flightsStat._1._2);
+                    return String.format("Stat from %s to %s: %s",
+                            origAirportName, destAirportName, flightsStat._2.toString())
                 }
         )
 
