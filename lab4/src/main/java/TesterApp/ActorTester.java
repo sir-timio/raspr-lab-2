25be16a594a05e1771ffec4acb7f3715641ba4ad
sir-timio <t-ionov@list.ru>
2021-12-10 22:21:30 +0300

    Scripted auto-commit on change (2021-12-10 22:21:30) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorTester.java
+++ lab4/src/main/java/TesterApp/ActorTester.java
@@ -2,6 +2,8 @@ package TesterApp;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+
 public class ActorTester extends AbstractActor {
     private static final String EVAL_ENGINE = "nashorn";
     private static final String PASSED_STATUS = "OK";
@@ -9,6 +11,6 @@ public class ActorTester extends AbstractActor {
     private static final String ERROR_STATUS = "ERROR";
 
 
-    private String evalJS(String jscript, String functionName, )
+    private String evalJS(String jscript, String functionName, ArrayList<Object> params) throws 
 
 }
