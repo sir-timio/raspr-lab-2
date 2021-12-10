a12fecbb7859f1c973a5789e4ad7d17dda301224
sir-timio <t-ionov@list.ru>
2021-12-10 23:55:39 +0300

    Scripted auto-commit on change (2021-12-10 23:55:39) by gitwatch.sh

--- lab4/src/main/java/TesterApp/MessageTestsPackageResult.java
+++ lab4/src/main/java/TesterApp/MessageTestsPackageResult.java
@@ -1,4 +1,13 @@
 package TesterApp;
 
 public class MessageTestsPackageResult {
+    private final String packageID;
+
+    public MessageGetTestPackageResult(String packageID) {
+        this.packageID = packageID;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
 }
