2d5ade01131be917c43f9910fd51c3ed23b17827
sir-timio <t-ionov@list.ru>
2021-12-10 21:39:22 +0300

    Scripted auto-commit on change (2021-12-10 21:39:22) by gitwatch.sh

--- lab4/src/main/java/TesterApp/TestApp.java
+++ lab4/src/main/java/TesterApp/TestApp.java
@@ -1,10 +1,12 @@
 package TesterApp;
+import akka.actor.ActorSystem;
 import akka.http.javadsl.server.AllDirectives;
 
 public class TestApp extends AllDirectives {
     private static final String ACTOR_SYSTEM_NAME = "JS_TESTER";
 
     public static void main(String[] args) {
+        ActorSystem actorSystem = ActorSystem.create(ACTOR_SYSTEM_NAME);
         
     }
 }
