316886a6d4cbcde9346279e20e1b5a56e674e584
sir-timio <t-ionov@list.ru>
2021-12-10 15:55:43 +0300

    Scripted auto-commit on change (2021-12-10 15:55:43) by gitwatch.sh

--- lab3/src/main/java/FlightApp/FlightStat.java
+++ lab3/src/main/java/FlightApp/FlightStat.java
@@ -26,6 +26,7 @@ public class FlightStat implements Serializable {
                 ONE_FLIGHT
         );
     }
+    
 
     public FlightStat updateStat(FlightStat stat, FlightSerializable flight) {
         return new FlightStat(
