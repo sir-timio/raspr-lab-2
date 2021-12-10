32e721eefbd20fef8326ca4291d8a6fd47ad155c
sir-timio <t-ionov@list.ru>
2021-12-10 22:12:12 +0300

    Scripted auto-commit on change (2021-12-10 22:12:12) by gitwatch.sh

--- lab4/src/main/java/TesterApp/TestResult.java
+++ lab4/src/main/java/TesterApp/TestResult.java
@@ -5,5 +5,7 @@ public class TestResult {
     private final String status;
     private final String testName;
     private final String expectedResult;
-    private final String
+    private final String evaluatedResult;
+
+    
 }
