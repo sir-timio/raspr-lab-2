62152b2bb55bbdf5fa61d9a854b42d427d495acb
sir-timio <t-ionov@list.ru>
2021-12-10 23:52:01 +0300

    Scripted auto-commit on change (2021-12-10 23:52:01) by gitwatch.sh

--- lab4/src/main/java/TesterApp/MessageTestsPackage.java
+++ lab4/src/main/java/TesterApp/MessageTestsPackage.java
@@ -1,4 +1,15 @@
 package TesterApp;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.List;
+
 public class MessageTestsPackage {
+    private final String packageId;
+    private final String jsScript;
+    private final String functionName;
+    private final List<Test> testList;
+
+    @JsonCreator
+    public MessageTestsPackage(@J)
 }
