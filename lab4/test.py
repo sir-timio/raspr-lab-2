9accc7ab4c80aec9dd20585297c7a63df458ef74
sir-timio <t-ionov@list.ru>
2021-12-17 21:19:26 +0300

    test

--- lab4/test.py
+++ lab4/test.py
@@ -1,19 +1,21 @@
 import http
 import json
+import time
 
 import requests
+
 
 def main():
     url = 'http://localhost:8080'
     with open('test.json', 'r') as f:
         test = json.loads(f.read())
-    get = f'{url}/result?packageId=11'
     r = requests.post(f'{url}/test', json=test)
     assert r.status_code == http.HTTPStatus.OK
-
-    r = requests.get(get)
+    time.sleep(1)
+    r = requests.get(f'{url}/result?packageId=11')
     assert r.status_code == http.HTTPStatus.OK
-    print(r)
+    print(r.content)
+
 
 if __name__ == '__main__':
     main()
