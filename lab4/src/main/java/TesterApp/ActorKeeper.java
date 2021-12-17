c74be990bc90319af0cd91c30f8a9b6900a224e4
sir-timio <t-ionov@list.ru>
2021-12-17 20:49:30 +0300

    Scripted auto-commit on change (2021-12-17 20:49:30) by gitwatch.sh

--- lab4/src/main/java/TesterApp/ActorKeeper.java
+++ lab4/src/main/java/TesterApp/ActorKeeper.java
@@ -17,7 +17,7 @@ public class ActorKeeper extends AbstractActor {
                         m -> {
                             String packageId = m.getPackageId();
                             if  (!results.containsKey(packageId)) {
-                                results.put(m.getPackageId(), new ArrayList<>());
+                                results.put(m.getPackageId(), new List<>());
                             }
                             results.get(packageId).add(m.getResult());
                         }
