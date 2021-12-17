21e15a2efee4a0174f48b72640fdec04e9cc5818
sir-timio <t-ionov@list.ru>
2021-12-17 19:16:25 +0300

    Scripted auto-commit on change (2021-12-17 19:16:25) by gitwatch.sh

---
+++ lab4/test.py
@@ -0,0 +1,19 @@
+import http
+import json
+
+import requests
+
+def main():
+    url = 'http://localhost:8080'
+    with open('test.json', 'r') as f:
+        test = json.loads(f.read())
+    get = f'{url}/result?packageId=11'
+    r = requests.post(f'{url}/test', json=test)
+    assert r.status_code == http.HTTPStatus.OK
+
+    r = requests.get(get)
+    assert r.status_code == http.HTTPStatus.OK
+    print(r)
+
+if __name__ == '__main__':
+    main()
