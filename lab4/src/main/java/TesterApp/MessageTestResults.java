e2b5adb04f54314ed2148219eae066698ed01a4b
sir-timio <t-ionov@list.ru>
2021-12-17 19:34:15 +0300

    Scripted auto-commit on change (2021-12-17 19:34:15) by gitwatch.sh

--- lab4/src/main/java/TesterApp/MessageTestResults.java
+++ lab4/src/main/java/TesterApp/MessageTestResults.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MessageTestResults {
     private static String packageId;
