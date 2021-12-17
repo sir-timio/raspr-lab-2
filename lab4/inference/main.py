a85f2455546d403f0af0440c17191b79e9dea4bf
sir-timio <t-ionov@list.ru>
2021-12-17 19:55:07 +0300

    Scripted auto-commit on change (2021-12-17 19:55:07) by gitwatch.sh

--- lab4/inference/main.py
+++ lab4/inference/main.py
@@ -7,7 +7,6 @@ def main():
     url = 'http://localhost:8080'
     with open('test.json', 'r') as f:
         test = json.loads(f.read())
-    get = f'{url}/result?packageId=11'
     r = requests.post(f'{url}/test', json=test)
     assert r.status_code == http.HTTPStatus.OK
 
