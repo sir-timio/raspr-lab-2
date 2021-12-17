f11de37cd002496ee33f523580e6960253bc554b
sir-timio <t-ionov@list.ru>
2021-12-17 20:16:48 +0300

    Scripted auto-commit on change (2021-12-17 20:16:48) by gitwatch.sh

--- lab4/src/main/java/TesterApp/TestResultStore.java
+++ lab4/src/main/java/TesterApp/TestResultStore.java
@@ -5,12 +5,14 @@ public class TestResultStore {
     private final String status;
     private final String testName;
     private final String expectedResult;
-    private final String evaluatedResult;
+    private final String receivedResult;
     public TestResultStore(String packageId, String status, String testName,
                            String expectedResult, String evaluatedResult) {
         this.packageId = packageId;
-        this.result = new TestResultJson(packageId, status,
-                testName, expectedResult, evaluatedResult);
+        this.status = status;
+        this.testName = testName,
+        this.expectedResult = expectedResult;
+        this.receivedResult = evaluatedResult;
     }
 
     public String getPackageId() {
