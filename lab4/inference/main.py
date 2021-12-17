748257972168d36cb168eab2580e62c3de2ea051
sir-timio <t-ionov@list.ru>
2021-12-17 16:35:37 +0300

    Scripted auto-commit on change (2021-12-17 16:35:37) by gitwatch.sh

--- lab4/inference/main.py
+++ lab4/inference/main.py
@@ -1,7 +1,22 @@
 import http
 
 def main():
-    post = 
+    post = """
+{
+"packageId":"11",
+"jsScript":"var divideFn = function(a,b) { return a/b} ",
+"functionName":"divideFn",
+"tests": [
+{"testName":"test1",
+"expectedResult":"2.0",
+"params":[2,1]
+},
+{"testName":"test2",
+"expectedResult":"2.0",
+"params":[4,2]
+}
+]
+}"""
 
 if __name__ == '__main__':
     main()
