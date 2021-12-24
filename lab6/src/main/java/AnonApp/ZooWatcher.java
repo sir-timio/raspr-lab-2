da3502fe12e29c116d6a8886802357880457d327
sir-timio <t-ionov@list.ru>
2021-12-24 21:38:31 +0300

    Scripted auto-commit on change (2021-12-24 21:38:31) by gitwatch.sh

--- lab6/src/main/java/AnonApp/ZooWatcher.java
+++ lab6/src/main/java/AnonApp/ZooWatcher.java
@@ -6,12 +6,15 @@ import org.apache.zookeeper.ZooKeeper;
 public class ZooWatcher implements Watcher {
     public static final String ZOO_HOST = "127.0.0.1";
     public static final String ZOO_PORT = "8000";
+    private static final int SESSION_TIMEOUT = 5000;
     private final ZooKeeper zooKeeper;
     private final ActorRef actorConfigKeeper;
 
     public ZooWatcher(ActorRef actorConfigKeeper, String host, Integer port) throws Exception {
         this.zooKeeper = new ZooKeeper(
-                ZOO_HOST + ": " + ZOO_PORT;
+                ZOO_HOST + ": " + ZOO_PORT,
+                10,
+                this
         )
     }
 
