3ff84a818463ec71031090de6638b9d82687e0b5
sir-timio <t-ionov@list.ru>
2021-12-24 21:38:00 +0300

    Scripted auto-commit on change (2021-12-24 21:38:00) by gitwatch.sh

--- lab6/src/main/java/AnonApp/ZooWatcher.java
+++ lab6/src/main/java/AnonApp/ZooWatcher.java
@@ -1,7 +1,19 @@
 package AnonApp;
+import akka.actor.ActorRef;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class ZooWatcher implements Watcher {
     public static final String ZOO_HOST = "127.0.0.1";
-    public static final String ZOO_PORT = "8000"
+    public static final String ZOO_PORT = "8000";
+    private final ZooKeeper zooKeeper;
+    private final ActorRef actorConfigKeeper;
+
+    public ZooWatcher(ActorRef actorConfigKeeper, String host, Integer port) throws Exception {
+        this.zooKeeper = new ZooKeeper(
+                ZOO_HOST + ": " + ZOO_PORT;
+        )
+    }
+
 }
+
