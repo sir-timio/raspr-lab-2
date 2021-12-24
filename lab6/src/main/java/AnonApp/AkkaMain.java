965bc4ae36f3f90bca9fd120bd3829005cf5be73
sir-timio <t-ionov@list.ru>
2021-12-24 21:52:26 +0300

    Scripted auto-commit on change (2021-12-24 21:52:26) by gitwatch.sh

--- lab6/src/main/java/AnonApp/AkkaMain.java
+++ lab6/src/main/java/AnonApp/AkkaMain.java
@@ -15,6 +15,7 @@ import akka.stream.ActorMaterializer;
 
 public class AkkaMain {
 
+    private static final String HOST = "127.0.0.1"
     private static final int SESSION_TIMEOUT = 5000;
 
     public static void main(String[] args) throws IOException {
