8513fae16829dd8436ca43f0a9a5e7f3850d13fc
sir-timio <t-ionov@list.ru>
2021-12-24 19:50:29 +0300

    Scripted auto-commit on change (2021-12-24 19:50:29) by gitwatch.sh

--- lab6/src/main/java/AkkaStream/Message/MessageServers.java
+++ lab6/src/main/java/AkkaStream/Message/MessageServers.java
@@ -1,2 +1,15 @@
-package AkkaStream.Message;public class MessageServerrs {
+package AkkaStream.Message;
+
+import java.util.ArrayList;
+
+public class MessageServers {
+    private final ArrayList<String> servers;
+
+    public MessageServers(ArrayList<String> servers) {
+        this.servers = servers;
+    }
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
 }
