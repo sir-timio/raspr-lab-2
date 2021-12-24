a6985d688c84ddbad748b28e661f56931dd64a40
sir-timio <t-ionov@list.ru>
2021-12-24 21:47:40 +0300

    Scripted auto-commit on change (2021-12-24 21:47:40) by gitwatch.sh

--- lab6/src/main/java/AnonApp/ZooWatcher.java
+++ lab6/src/main/java/AnonApp/ZooWatcher.java
@@ -33,7 +33,7 @@ public class ZooWatcher implements Watcher {
     public void process(WatchedEvent watchedEvent) {
         try {
             zooKeeper.getChildren(SERVERS_PATH, this);
-            se
+            sendServers();
         }
     }
 }
