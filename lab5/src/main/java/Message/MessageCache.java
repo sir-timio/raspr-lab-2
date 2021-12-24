2ef800e05fc4d8e0e339d9f224cee6692ad6a8b8
sir-timio <t-ionov@list.ru>
2021-12-24 16:23:17 +0300

    Scripted auto-commit on change (2021-12-24 16:23:17) by gitwatch.sh

--- lab5/src/main/java/Message/MessageCache.java
+++ lab5/src/main/java/Message/MessageCache.java
@@ -3,4 +3,17 @@ package Message;
 public class MessageCache {
     private final String url;
     private final long responseTime;
+
+    public MessageCache(String url, Long responseTime) {
+        this.url = url;
+        this.responseTime = responseTime;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public long getResponseTime() {
+        return responseTime;
+    }
 }
