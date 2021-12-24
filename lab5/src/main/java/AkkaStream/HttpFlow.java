42fc0e47fa1465142ff2e0e8354c5f1b66971014
sir-timio <t-ionov@list.ru>
2021-12-24 17:28:06 +0300

    Scripted auto-commit on change (2021-12-24 17:28:06) by gitwatch.sh

--- lab5/src/main/java/AkkaStream/HttpFlow.java
+++ lab5/src/main/java/AkkaStream/HttpFlow.java
@@ -1,12 +1,25 @@
 package AkkaStream;
 
 import akka.NotUsed;
+import akka.actor.Actor;
+import akka.actor.ActorRef;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class HttpFlow {
     public static final TIMEOUT = 5;
     public static final NUM_OF_WORKERS = 3;
 
-    public static Flow<HttpRequest, HttpResponse, NotUsed
+    public static Flow<HttpRequest, HttpResponse, NotUsed> httpFlow(
+            ActorMaterializer materializer, ActorRef actorRef
+    ) {
+        return Flow.of(HttpRequest.class)
+                .map(request -> {
+                    Query query request.getUri().query();
+                    return new P
+                })
+    }
 }
