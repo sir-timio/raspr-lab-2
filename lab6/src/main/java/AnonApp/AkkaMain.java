3d65a0dbdc0e4985a47a1e544a62b606c55c8530
sir-timio <t-ionov@list.ru>
2021-12-25 01:21:03 +0300

    Scripted auto-commit on change (2021-12-25 01:21:03) by gitwatch.sh

--- lab6/src/main/java/AnonApp/AkkaMain.java
+++ lab6/src/main/java/AnonApp/AkkaMain.java
@@ -28,7 +28,7 @@ import org.apache.zookeeper.ZooKeeper;
 public class AkkaMain {
 
     private static final String HOST = "127.0.0.1";
-    private static final String ZOO_PORT = "8000";
+    private static final String ZOO_PORT = "2181";
     private static final int SESSION_TIMEOUT = 5000;
 
     public static void main(String[] args) throws Exception {
