3abf577060000b52b5ff409625042fc64771b962
sir-timio <t-ionov@list.ru>
2021-12-24 18:35:53 +0300

    Scripted auto-commit on change (2021-12-24 18:35:53) by gitwatch.sh

--- lab5/src/main/java/AkkaStream/AkkaMain.java
+++ lab5/src/main/java/AkkaStream/AkkaMain.java
@@ -1,14 +1,20 @@
 package AkkaStream;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 import AkkaStream.Actor.ActorKeeper;
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 
 public class AkkaMain {
@@ -20,12 +26,12 @@ public class AkkaMain {
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = flowHttpRequest(materializer, actor);
-//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-////                routelow,
-//                ConnectHttp.toHost("localhost", 8080),
-//                materializer
-//        );
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = HttpFlow.httpFlow(materializer, actor);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
