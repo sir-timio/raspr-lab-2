45ca6e57ca4d547d7caa5f11896891abf00a2d23
sir-timio <t-ionov@list.ru>
2021-12-24 16:06:32 +0300

    Scripted auto-commit on change (2021-12-24 16:06:32) by gitwatch.sh

--- lab5/src/main/java/AkkaStream/AkkaMain.java
+++ lab5/src/main/java/AkkaStream/AkkaMain.java
@@ -1,8 +1,28 @@
 package AkkaStream;
 
+import java.io.IOException;
+
 public class AkkaMain {
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        ActorRef actor = system.actorOf(Props.create(ActorCache.class));
 
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = flowHttpRequest(materializer, actor);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", PORT),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:" + PORT + "/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+    }
 
 
 }
