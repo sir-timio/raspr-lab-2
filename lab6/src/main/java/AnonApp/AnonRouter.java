a724fe4268fd1d5f055cb10ff7d9008414823ef8
sir-timio <t-ionov@list.ru>
2021-12-24 20:19:24 +0300

    Scripted auto-commit on change (2021-12-24 20:19:24) by gitwatch.sh

--- lab6/src/main/java/AnonApp/AnonRouter.java
+++ lab6/src/main/java/AnonApp/AnonRouter.java
@@ -2,6 +2,7 @@ package AnonApp;
 
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.server.Route;
 
 public class AnonRouter {
     private final ActorRef actorConfigKeeper;
@@ -11,6 +12,10 @@ public class AnonRouter {
     public AnonRouter(ActorRef actorConfigKeeper, Http client) {
         this.actorConfigKeeper = actorConfigKeeper;
         this.client = client;
+    }
+
+    public Route createRoute() {
+        return route()
     }
 
 }
