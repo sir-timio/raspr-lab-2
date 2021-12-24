836c00f39396d0b872aa00d379f966f8d3a097f1
sir-timio <t-ionov@list.ru>
2021-12-24 17:24:16 +0300

    Scripted auto-commit on change (2021-12-24 17:24:16) by gitwatch.sh

--- lab5/src/main/java/AkkaStream/HttpFlow.java
+++ lab5/src/main/java/AkkaStream/HttpFlow.java
@@ -1,7 +1,12 @@
 package AkkaStream;
 
+import akka.NotUsed;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+
 public class HttpFlow {
     public static final TIMEOUT = 5;
     public static final NUM_OF_WORKERS = 3;
-    
+
+    public static Flow<HttpRequest, HttpResponse, NotUsed
 }
