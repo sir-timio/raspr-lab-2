8fc1f3233d8d7a2c113c5fc5726b76163d06bd02
sir-timio <t-ionov@list.ru>
2021-12-24 21:35:59 +0300

    Scripted auto-commit on change (2021-12-24 21:35:59) by gitwatch.sh

--- lab6/src/main/java/AnonApp/ZooWatcher.java
+++ lab6/src/main/java/AnonApp/ZooWatcher.java
@@ -2,4 +2,6 @@ package AnonApp;
 import org.apache.zookeeper.Watcher;
 
 public class ZooWatcher implements Watcher {
+    public static final String ZOO_HOST = "127.0.0.1";
+    public static final String ZOO_PORT = "8000"
 }
