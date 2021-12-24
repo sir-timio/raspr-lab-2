62a26996a6bf11720e8e96eb03ac3bb333dcdb60
sir-timio <t-ionov@list.ru>
2021-12-24 20:24:19 +0300

    Scripted auto-commit on change (2021-12-24 20:24:19) by gitwatch.sh

--- lab6/src/main/java/AnonApp/AnonRouter.java
+++ lab6/src/main/java/AnonApp/AnonRouter.java
@@ -10,6 +10,7 @@ public class AnonRouter {
     private final ActorRef actorConfigKeeper;
     private final Http client;
     private final int TIMEOUT_SECONDS = 5;
+    private static final ZERO_STRING = "0";
 
     public AnonRouter(ActorRef actorConfigKeeper, Http client) {
         this.actorConfigKeeper = actorConfigKeeper;
