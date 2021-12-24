1a5075768e3659fff14a5d12a8d9d8bcd26f040f
sir-timio <t-ionov@list.ru>
2021-12-24 19:52:43 +0300

    Scripted auto-commit on change (2021-12-24 19:52:43) by gitwatch.sh

--- lab6/src/main/java/AkkaStream/Actor/ActorConfigKeeper.java
+++ lab6/src/main/java/AkkaStream/Actor/ActorConfigKeeper.java
@@ -1,4 +1,18 @@
 package AkkaStream.Actor;
 
+import AkkaStream.Message.MessageServers;
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+
 public class ActorConfigKeeper {
+    private ArrayList<String> servers;
+
+    @Override
+    public AbstractActor.Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(MessageServers.class, )
+
+    }
 }
