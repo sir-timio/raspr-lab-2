63e623160c4403c9fc3478454a336cc46ba017df
sir-timio <t-ionov@list.ru>
2021-12-24 20:04:43 +0300

    Scripted auto-commit on change (2021-12-24 20:04:43) by gitwatch.sh

--- lab6/src/main/java/AkkaStream/Actor/ActorConfigKeeper.java
+++ lab6/src/main/java/AkkaStream/Actor/ActorConfigKeeper.java
@@ -22,7 +22,7 @@ public class ActorConfigKeeper extends AbstractActor{
                                         Actor.noSender()
                                 )
                 )
-                .match()
+                .match(MessageRandom)
 
     }
 }
